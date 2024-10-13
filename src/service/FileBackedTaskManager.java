a16875bcc715a7bf24079477adf3fe9d9a36f615
ox7193 <oxana-dubiaga@yandex.ru>
2024-10-13 10:38:47 +0300

    ФЗ спринта 7. Добавлены классы FileBackedTaskManager и ManagerSaveException для работы с файлами. Исправлен UnusedImports

--- src/service/FileBackedTaskManager.java
+++ src/service/FileBackedTaskManager.java
@@ -10,8 +10,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class FileBackedTaskManager extends InMemoryTaskManager implements TaskManager {
